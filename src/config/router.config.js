decb034f65e3551e572ac1f533afab4fd9dd4d42
wong <1457368987@qq.com>
2019-12-11 18:15:17 +0800

    增加首页

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -9,17 +9,23 @@ export const asyncRouterMap = [
     name: 'index',
     component: BasicLayout,
     meta: { title: '首页' },
-    redirect: '/dashboard/analysis',
+    redirect: '/dashboard/home',
     children: [
       // dashboard
       {
         path: '/dashboard',
         name: 'dashboard',
-        redirect: '/dashboard/analysis',
+        redirect: '/dashboard/home',
         component: RouteView,
-        hideChildrenInMenu: true,
+        // hideChildrenInMenu: true,
         meta: { title: '立体巡河实况', keepAlive: true, icon: bxAnaalyse, permission: [ 'dashboard' ] },
         children: [
+          {
+            path: '/dashboard/home',
+            name: 'Home',
+            component: () => import('@/views/dashboard/Home'),
+            meta: { title: '首页', keepAlive: true, permission: [ 'dashboard' ] }
+          },
           {
             path: '/dashboard/analysis',
             name: 'Analysis',
