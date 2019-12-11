5b4af7cf5fa40e9df46ec5d401c2c2f5da1ef673
wong <1457368987@qq.com>
2019-12-11 18:21:50 +0800

    进入首页

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -213,7 +213,7 @@ export default {
       */
       // this.$router.push({ name: 'analysis' })
       this.$router.push({
-        path: '/dashboard/analysis'
+        path: '/dashboard/home'
       })
       // 延迟 1 秒显示欢迎信息
       setTimeout(() => {
