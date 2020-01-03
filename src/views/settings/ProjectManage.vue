38d961562af8186cf66a32b36387a9e891ea4ff1
wong <1457368987@qq.com>
2020-01-03 19:44:37 +0800

    项目设置中心点

--- src/views/settings/ProjectManage.vue
+++ src/views/settings/ProjectManage.vue
@@ -9,7 +9,7 @@
       <div style="display:flex;width:100%;box-sizing:border-box;">
         <div style="width:220px">
           <a-card style="min-height: 200px">
-            <el-tree :data="treeData"  @node-click="select"></el-tree>
+            <el-tree :data="treeData" @node-click="select"></el-tree>
           </a-card>
         </div>
         <div style="width:100%;margin-left:20px">
@@ -32,15 +32,10 @@
             </a-table>
           </div>
           <div v-else>
-            <a-button
-              type="primary"
-              icon="plus"
-              @click="equipmentModel=true"
-              style="margin-bottom:15px"
-            >添加</a-button>
+            <a-button type="primary" icon="plus" @click="addProject" style="margin-bottom:15px">添加</a-button>
             <a-table :columns="columns1" :dataSource="data1" bordered>
               <template slot="operation" slot-scope="row">
-                <a @click="add1(row.id,row.name,row.interval.code)">编辑</a>
+                <a @click="add1(row)">编辑</a>
                 <a-divider type="vertical" />
                 <a-popconfirm
                   title="是否确认删除?"
@@ -57,41 +52,93 @@
         </div>
       </div>
     </a-card>
-    <a-modal title="添加一级项目" v-model="visible"
+    <a-modal
+      title="添加一级项目"
+      :maskClosable="false"
+      :centered="true"
+      v-model="visible"
       @ok="handleOk"
       @cancel="handleCancel"
     >
-      <el-form ref="formValidate" :model="equipmentList" :rules="ruleValidate" >
-        <el-form-item label="项目名称" prop="name">
-          <el-input v-model="equipmentList.name" placeholder="请输入" style="width:200px" />
+      <el-form ref="formValidate" :model="equipmentList" :rules="ruleValidate">
+        <el-form-item label="项目名称" prop="projectParentName">
+          <el-input
+            v-model="equipmentList.projectParentName"
+            placeholder="请输入"
+            style="width:200px"
+          />
         </el-form-item>
       </el-form>
     </a-modal>
-    <a-modal title="添加二级项目" v-model="equipmentModel"
+    <a-modal
+      title="添加二级项目"
+      :maskClosable="false"
+      :destroyOnClose="true"
+      :width="980"
+      :centered="true"
+      v-model="equipmentModel"
       @ok="handleOk1"
       @cancel="handleCancel1"
     >
-      <el-form ref="formValidate" :model="equipmentList" :rules="ruleValidate" label-width="100px" >
-        <el-form-item label="上级项目" prop="name">
-          <el-input v-model="equipmentList.name" disabled placeholder="请输入" style="width:200px" />
-        </el-form-item>
-        <el-form-item label="项目名称" prop="type">
-          <el-input v-model="equipmentList.type" placeholder="请输入" style="width:200px" />
-        </el-form-item>
-        <el-form-item label="间隔周期">
-          <a-select style="width: 200px" placeholder="请选择"  v-model="equipmentList.number">
-            <a-select-option value="day">日</a-select-option>
-            <a-select-option value="week">周</a-select-option>
-            <a-select-option value="month">月</a-select-option>
-          </a-select>
-        </el-form-item>
+      <el-form ref="formValidate" :model="equipmentList" :rules="ruleValidate" label-width="100px">
+        <a-row>
+          <a-col :span="12">
+            <el-form-item label="上级项目" prop="projectParentName">
+              <el-input
+                v-model="equipmentList.projectParentName"
+                disabled
+                placeholder="请输入"
+                style="width:240px"
+              />
+            </el-form-item>
+          </a-col>
+          <a-col :span="12">
+            <el-form-item label="项目名称" prop="projectName">
+              <el-input v-model="equipmentList.projectName" placeholder="请输入" style="width:240px" />
+            </el-form-item>
+          </a-col>
+          <a-col :span="12">
+            <el-form-item label="项目中心" prop="projectParentName">
+              <el-input
+                v-model="equipmentList.latlng"
+                disabled
+                placeholder="请在地图选择"
+                style="width:240px"
+              >
+                <el-button
+                  slot="append"
+                  @click="projectCoordinate"
+                  style="font-size: 23px;"
+                  icon="el-icon-location-outline"
+                ></el-button>
+              </el-input>
+            </el-form-item>
+          </a-col>
+          <a-col :span="12">
+            <el-form-item label="间隔周期">
+              <a-select style="width: 240px" v-model="equipmentList.number">
+                <a-select-option value="day">日</a-select-option>
+                <a-select-option value="week">周</a-select-option>
+                <a-select-option value="month">月</a-select-option>
+              </a-select>
+            </el-form-item>
+          </a-col>
+        </a-row>
       </el-form>
+      <div id="map" class="map_wrap"></div>
     </a-modal>
   </div>
 </template>
 
 <script>
-import {  projectMinesTructure,projectTypeSave,projectTypeDel,projectNewsList,projectNewsSave,projectNewsDel } from '@/api/login'
+import {
+  projectMinesTructure,
+  projectTypeSave,
+  projectTypeDel,
+  projectNewsList,
+  projectNewsSave,
+  projectNewsDel
+} from '@/api/login'
 const treeData = []
 const columns = [
   {
@@ -132,12 +179,12 @@ const columns1 = [
 export default {
   data() {
     return {
-      id:'',
+      id: '',
       treeId: true,
       treeData,
       columns,
       columns1,
-      data:[],
+      data: [],
       visible: false,
       equipmentModel: false,
       typeList: {
@@ -145,174 +192,229 @@ export default {
         typeName: ''
       },
       equipmentList: {
-        id:'',
-        name: '',
-        type: '',
-        number: '',
-        typeId:'',
+        id: '',
+        projectParentName: '',
+        projectName: '',
+        number: 'day',
+        projectNameId: '',
+        latlng: ''
       },
       ruleValidate: {
-        name: [{ required: true, message: '不能为空', trigger: 'blur' }],
-        type: [{ required: true, message: '不能为空', trigger: 'blur' }]
+        projectParentName: [{ required: true, message: '不能为空', trigger: 'blur' }],
+        projectName: [{ required: true, message: '不能为空', trigger: 'blur' }]
       },
-      data1: [
-        
-      ]
+      data1: [],
+      map: {}
     }
   },
   watch: {
-    $route(){
+    $route() {
       this.getList()
-    },
+    }
   },
-  mounted(){
+  mounted() {
     this.getList()
   },
   methods: {
-    getList(){
-      this.treeData=[{
-        label: '全部',
-        id: '0',
-        code:'1',
-        children: [
-        
-        ]
-      }]
-      var data={
-        id:'0'
+    projectCoordinate() {
+      console.log(333)
+      if (this.cp) {
+        this.cp.removeEvent()
       }
-      projectMinesTructure(data).then(res => {
-         var arr = res.data
-        for (let i = 0; i < arr.length; i++) {
-           arr[i].key=i+1
-           arr[i].code='2'
-           arr[i].label=arr[i].name
-           arr[i].children =[]
+      this.cp = new T.CoordinatePickup(this.map, { callback: this.getLngLat })
+      this.cp.addEvent()
+    },
+    getLngLat(lnglat) {
+      console.log(lnglat.lng + ', ' + lnglat.lat)
+      this.equipmentList.latlng = lnglat.lng + ', ' + lnglat.lat
+    },
+    getList() {
+      this.treeData = [
+        {
+          label: '全部',
+          id: '0',
+          code: '1',
+          children: []
         }
-        for (let i = 0; i < arr.length; i++) {
-          this.treeData[0].children.push(arr[i])
-        }
-         this.data=arr
-        }).catch(err => {
-
+      ]
+      var data = {
+        id: '0'
+      }
+      projectMinesTructure(data)
+        .then(res => {
+          var arr = res.data
+          for (let i = 0; i < arr.length; i++) {
+            arr[i].key = i + 1
+            arr[i].code = '2'
+            arr[i].label = arr[i].name
+            arr[i].children = []
+          }
+          for (let i = 0; i < arr.length; i++) {
+            this.treeData[0].children.push(arr[i])
+          }
+          this.data = arr
+        })
+        .catch(err => {})
+    },
+    addProject() {
+      this.equipmentModel = true
+      this.$nextTick(() => {
+        // 初始化地图控件
+        let zoom = 14
+        let twoDimensionURL =
+          'http://t0.tianditu.com/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=a659a60049b130a5d1fececfd5a6b822'
+        this.mapLayer2d = new T.TileLayer(twoDimensionURL, { minZoom: 4, maxZoom: 18, zIndex: 10 })
+        let satelliteURL =
+          'http://t0.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=a659a60049b130a5d1fececfd5a6b822'
+        this.mapLayerSatellite = new T.TileLayer(satelliteURL, { minZoom: 4, maxZoom: 18, zIndex: 10 })
+        // 创建自定义图层对象
+        let wordLabel =
+          'http://t0.tianditu.com/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=a659a60049b130a5d1fececfd5a6b822'
+        this.mapLayerWord = new T.TileLayer(wordLabel, { minZoom: 4, maxZoom: 18, zIndex: 15 })
+        // 正射影像
+        // this.mapImage = `${this.host}/server/data/admin/regulator/uav/data/mbtiles?year=${this.mapYear}&month=${this.mapMonth}&day=${this.mapDay}&x={x}&y={y}&z={z}&X-TENANT-ID=jl:jlgis@2019&Authorization=${token}`
+        // this.mapLayerImage = new T.TileLayer(this.mapImage, { minZoom: 4, maxZoom: 23, zIndex: 12 })
+        this.map = new T.Map('map', {
+          minZoom: 4,
+          maxZoom: 23,
+          layers: [this.mapLayerSatellite, this.mapLayerWord]
+        })
+        this.map.centerAndZoom(new T.LngLat(121.43429, 31.15847), zoom)
       })
     },
-    add(id,name){
-      this.equipmentList.id=id
-      this.equipmentList.name=name
-      this.visible = true;
+    add(id, name) {
+      this.equipmentList.id = id
+      this.equipmentList.projectParentName = name
+      this.visible = true
     },
     handleOk(e) {
-      var data ={
-        id:this.equipmentList.id,
-        name:this.equipmentList.name
+      var data = {
+        id: this.equipmentList.id,
+        name: this.equipmentList.projectParentName
       }
-      projectTypeSave(data).then(res => {
-          this.$message.success('保存成功');
-          this.visible = false;
-          this.equipmentList.id=''
-          this.equipmentList.name=''
+      projectTypeSave(data)
+        .then(res => {
+          this.$message.success('保存成功')
+          this.visible = false
+          this.equipmentList.id = ''
+          this.equipmentList.projectParentName = ''
           this.getList()
-        }).catch(err => {
-          this.$message.error(err.response.data.message);
-      })
+        })
+        .catch(err => {
+          this.$message.error(err.response.data.message)
+        })
     },
     handleCancel(e) {
-      this.equipmentList.id=''
-      this.equipmentList.name=''
-      this.visible = false;
+      this.equipmentList.id = ''
+      this.equipmentList.projectParentName = ''
+      this.visible = false
     },
-    add1(id,name,code){
-      this.equipmentList.id=id
-      this.equipmentList.type=name
-      this.equipmentList.number=code
-      this.equipmentModel = true;
+    add1(row) {
+      console.log(row)
+      this.equipmentList.id = row.id
+      // this.equipmentList.projectParentName = projectParentName
+      this.equipmentList.projectName = row.name
+      this.equipmentList.number = row.interval.code
+      this.addProject()
     },
     handleOk1(e) {
-      var data ={
-        id:this.equipmentList.id,
-        name:this.equipmentList.type,
-        interval:this.equipmentList.number,
-        typeId:this.equipmentList.typeId
+      console
+      if (!this.equipmentList.projectName) {
+        this.$message.error('项目名不能为空')
+      } else if (!this.equipmentList.latlng) {
+        this.$message.error('请选择项目中心')
+      } else {
+        var data = {
+          id: this.equipmentList.id,
+          name: this.equipmentList.projectName,
+          interval: this.equipmentList.number,
+          typeId: this.equipmentList.projectNameId,
+          coordinate: this.equipmentList.latlng
+        }
+        projectNewsSave(data)
+          .then(res => {
+            this.$message.success('保存成功')
+            this.handleCancel1()
+            this.newList()
+          })
+          .catch(err => {
+            this.$message.error(err.response.data.message)
+          })
       }
-      projectNewsSave(data).then(res => {
-          this.$message.success('保存成功');
-          this.handleCancel1()
-          this.newList()
-        }).catch(err => {
-          this.$message.error(err.response.data.message);
-      })
     },
     handleCancel1(e) {
-      this.equipmentList.id=''
-      this.equipmentList.type=''
-      this.equipmentList.number=''
-      this.equipmentModel = false;
+      this.equipmentList.id = ''
+      this.equipmentList.projectName = ''
+      this.equipmentList.number = ''
+      this.equipmentModel = false
     },
-    newList(){
-      var data={
-        id:this.id
+    newList() {
+      var data = {
+        id: this.id
       }
-      projectNewsList(data).then(res => {
-        var arr = res.data.data     
-        for (let i = 0; i < arr.length; i++) {
-           arr[i].key=i+1
-           arr[i].code='3'
-           arr[i].label=arr[i].name
-        }
-        this.data1=arr
-        // var children=this.treeData[0].children
-        // for (let i = 0; i < children.length; i++) {
-        //   if (children[i].id==this.id) {
-        //     children[i].children=new Array()
-        //     for (let a = 0; a < arr.length; a++) {
-        //       children[i].children.push(arr[a])
-        //     }
-        //   }
-        // }
-        }).catch(err => {
-
-      })
+      projectNewsList(data)
+        .then(res => {
+          var arr = res.data.data
+          for (let i = 0; i < arr.length; i++) {
+            arr[i].key = i + 1
+            arr[i].code = '3'
+            arr[i].label = arr[i].name
+          }
+          this.data1 = arr
+          // var children=this.treeData[0].children
+          // for (let i = 0; i < children.length; i++) {
+          //   if (children[i].id==this.id) {
+          //     children[i].children=new Array()
+          //     for (let a = 0; a < arr.length; a++) {
+          //       children[i].children.push(arr[a])
+          //     }
+          //   }
+          // }
+        })
+        .catch(err => {})
     },
     select(e) {
       // console.log(this.treeData);
-      console.log(e);
-      if (e.code=='1') {
-        this.treeId=true
-      }else if(e.code=='2') {
-        this.treeId=false
-        this.equipmentList.name=e.name
-        this.equipmentList.typeId=e.id
-        this.id=e.id
+      console.log(e)
+      if (e.code == '1') {
+        this.treeId = true
+      } else if (e.code == '2') {
+        this.treeId = false
+        console.log(e.name)
+        this.equipmentList.projectParentName = e.name
+        this.equipmentList.projectNameId = e.id
+        this.id = e.id
         this.newList()
       }
     },
-    del(id){
-      this.id1=id
+    del(id) {
+      this.id1 = id
     },
     confirm(e) {
-      var data ={
-        id:this.id1
+      var data = {
+        id: this.id1
       }
-      if (e=='1') {
-         projectTypeDel(data).then(res => {
-          this.$message.success('删除成功');
-          this.getList()
-        }).catch(err => {
-          this.$message.error(err.response.data.message);
-        })
-      }else{
-        projectNewsDel(data).then(res => {
-          this.$message.success('删除成功');
-          this.newList()
-        }).catch(err => {
-          this.$message.error(err.response.data.message);
-        })
+      if (e == '1') {
+        projectTypeDel(data)
+          .then(res => {
+            this.$message.success('删除成功')
+            this.getList()
+          })
+          .catch(err => {
+            this.$message.error(err.response.data.message)
+          })
+      } else {
+        projectNewsDel(data)
+          .then(res => {
+            this.$message.success('删除成功')
+            this.newList()
+          })
+          .catch(err => {
+            this.$message.error(err.response.data.message)
+          })
       }
     },
-    cancel(e) {
-
-    },
+    cancel(e) {},
     handleEdit(row) {
       this.visible = true
       this.typeList.id = row.id
@@ -326,5 +428,9 @@ export default {
 <style lang="less" scoped>
 .boder-tree {
   border: 1px solid #b8b8b8;
+}
+.map_wrap {
+  width: 100%;
+  height: 300px;
 }
 </style>
