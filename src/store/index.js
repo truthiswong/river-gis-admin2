22a44941bd4940c90bf6899cad8bdcf0fc499b4d
wong <1457368987@qq.com>
2020-01-06 15:07:24 +0800

    项目info, 项目中心点

--- src/store/index.js
+++ src/store/index.js
@@ -22,7 +22,12 @@ export default new Vuex.Store({
     isTestUrl: true, //默认使用测试服
     testServerUrl: 'http://demo-jleco-river.jl-shgroup.com',
     prodServerUrl: 'http://jleco-river.jl-shgroup.com',
-    tenantId: "test:jlgis@2019" // 默认测试
+    tenantId: "test:jlgis@2019", // 默认测试
+    projectCoordinate: { //项目坐标
+      lng: 0,
+      lat: 0
+    },
+    projectInfo: {}, //项目信息
   },
   mutations: {
     show(state, objid) {
@@ -34,9 +39,19 @@ export default new Vuex.Store({
     IS_TEST_URL: (state, index) => {
       state.isTestUrl = index
     },
+    // 商户签名
     tenantIdFn: (state, index) => {
       state.tenantId = index
-    }
+    },
+    // 项目坐标
+    projectCoordinateFn: (state, index) => {
+      state.projectCoordinate.lng = index[0]
+      state.projectCoordinate.lat = index[1]
+    },
+    // 项目信息
+    projectInfoFn: (state, index) => {
+      state.projectInfo = index
+    },
   },
   actions: {
 
