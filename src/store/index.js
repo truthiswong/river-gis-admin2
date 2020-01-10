d1c76afc553e0ae5d0fd2977d658b3a06135fc53
wong <1457368987@qq.com>
2020-01-10 19:56:02 +0800

    权鉴列表

--- src/store/index.js
+++ src/store/index.js
@@ -26,6 +26,124 @@ export default new Vuex.Store({
       lat: 0
     },
     projectInfo: {}, //项目信息
+    permissionLists: [
+      [
+        {
+          id: '5e142f3273e44f2d59d43766',
+          name: '巡河',
+          flag: 'xh',
+          read: false,
+          write: false
+        }
+      ],
+      [
+        {
+          id: '5e142f8373e44f2d59d43767',
+          name: '监管',
+          flag: 'jg',
+          read: false,
+          write: false
+        }
+      ],
+      [
+        {
+          id: '5e144bdfa96a47239fbf9d72',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '河道',
+          flag: 'river',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e15857aa96a47239fbf9dc5',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '街道',
+          flag: 'street',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e15858ba96a47239fbf9dc6',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '任务',
+          flag: 'task',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e1585b3a96a47239fbf9dc7',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '巡河方案',
+          flag: 'plan',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e1585d5a96a47239fbf9dc8',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '水质监测点',
+          flag: 'station',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e1587f4a96a47239fbf9dc9',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '督办单',
+          flag: 'supervision',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e158f06a96a47239fbf9dcf',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '设备',
+          flag: 'device',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e158f26a96a47239fbf9dd0',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '风险源类型',
+          flag: 'riskSourceType',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e158f37a96a47239fbf9dd1',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '绘制类型',
+          flag: 'drawType',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e158f4ba96a47239fbf9dd2',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '项目',
+          flag: 'project',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e158f58a96a47239fbf9dd3',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '角色',
+          flag: 'role',
+          read: false,
+          write: false
+        },
+        {
+          id: '5e158f67a96a47239fbf9dd4',
+          parentId: '5e142f8e73e44f2d59d43768',
+          name: '用户',
+          flag: 'user',
+          read: false,
+          write: false
+        }
+      ]
+    ]
   },
   mutations: {
     show(state, objid) {
@@ -50,9 +168,11 @@ export default new Vuex.Store({
     projectInfoFn: (state, index) => {
       state.projectInfo = index
     },
+    permissionListsFn: (state, index) => {
+      console.log(index)
+      state.permissionLists = index
+    },
   },
-  actions: {
-
-  },
+  actions: {},
   getters
 })
