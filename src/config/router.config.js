bf592ce5b7ae2c5bfd321910e0ec36151ae8eb2a
wong <1457368987@qq.com>
2020-01-10 19:55:25 +0800

    权鉴

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line
 import { UserLayout, BasicLayout, RouteView, BlankLayout, PageView } from '@/layouts'
 import { bxAnaalyse } from '@/core/icons'
-
 export const asyncRouterMap = [
 
   {
@@ -41,19 +40,19 @@ export const asyncRouterMap = [
         component: RouteView,
         redirect: '/supervise/Supervise',
         hideChildrenInMenu: true,
-        meta: { title: '数据统计分析', keepAlive: true, icon: 'eye', permission: [ 'dashboard' ] },
+        meta: { title: '数据统计分析', keepAlive: true, icon: 'eye', permission: [ 'supervise' ] },
         children: [
           {
             path: '/supervise/Supervise',
             name: 'Supervise',
             component: () => import('@/views/supervise/Supervise'),
-            meta: { title: '数据统计分析', permission: [ 'dashboard' ] }
+            meta: { title: '数据统计分析', permission: [ 'supervise' ] }
           },
           {
             path: '/supervise/Vtour',
             name: 'Vtour',
             component: () => import('@/views/supervise/Vtour'),
-            meta: { title: '360全景图', permission: [ 'dashboard' ] }
+            meta: { title: '360全景图', permission: [ 'supervise' ] }
           }
         ]
       },
@@ -63,82 +62,82 @@ export const asyncRouterMap = [
         name: 'settings',
         component: RouteView,
         redirect: '/settings/RiverManage',
-        meta: { title: '设置', keepAlive: true, icon: 'setting', permission: [ 'dashboard' ] },
+        meta: { title: '设置', keepAlive: true, icon: 'setting', permission: [ 'setting' ] },
         children: [
           {
             path: '/settings/RiverManage',
             name: 'RiverManage',
             // hideChildrenInMenu: true, // 强制显示 MenuItem 而不是 SubMenu
             component: () => import('@/views/settings/RiverManage'),
-            meta: { title: '河道管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '河道管理', keepAlive: false, permission: [ 'river' ] }
           },
           {
             path: '/settings/StreetManage',
             name: 'StreetManage',
             component: () => import('@/views/settings/StreetManage'),
-            meta: { title: '街道管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '街道管理', keepAlive: false, permission: [ 'street' ] }
           },
           {
             path: '/settings/TaskManage',
             name: 'TaskManage',
             component: () => import('@/views/settings/TaskManage'),
-            meta: { title: '任务管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '任务管理', keepAlive: false, permission: [ 'task' ] }
           },
           {
             path: '/settings/RiverPlanManage',
             name: 'RiverPlanManage',
             // hidden: false,
             component: () => import('@/views/settings/RiverPlanManage'),
-            meta: { title: '巡河方案管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '巡河方案管理', keepAlive: false, permission: [ 'plan' ] }
           },
           {
             path: '/settings/WaterPointManage',
             name: 'WaterPointManage',
             // hidden: false,
             component: () => import('@/views/settings/WaterPointManage'),
-            meta: { title: '水质监测点管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '水质监测点管理', keepAlive: false, permission: [ 'station' ] }
           },
           {
             path: '/settings/SupervisoryUnitsManage',
             name: 'SupervisoryUnitsManage',
             component: () => import('@/views/settings/SupervisoryUnitsManage'),
-            meta: { title: '督办单管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '督办单管理', keepAlive: false, permission: [ 'supervision' ] }
           },
           {
             path: '/settings/DeviceManage',
             name: 'DeviceManage',
             component: () => import('@/views/settings/DeviceManage'),
-            meta: { title: '设备管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '设备管理', keepAlive: false, permission: [ 'device' ] }
           },
           {
             path: '/settings/RiskTypeManage',
             name: 'RiskTypeManage',
             component: () => import('@/views/settings/RiskTypeManage'),
-            meta: { title: '风险源类型管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '风险源类型管理', keepAlive: false, permission: [ 'riskSourceType' ] }
           },
           {
             path: '/settings/drawManage',
             name: 'drawManage',
             component: () => import('@/views/settings/drawManage'),
-            meta: { title: '绘制类型管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '绘制类型管理', keepAlive: false, permission: [ 'drawType' ] }
           },
           {
             path: '/settings/ProjectManage',
             name: 'ProjectManage',
             component: () => import('@/views/settings/ProjectManage'),
-            meta: { title: '项目管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '项目管理', keepAlive: false, permission: [ 'project' ] }
           },
           {
             path: '/settings/UserRootManage',
             name: 'UserRootManage',
             component: () => import('@/views/settings/UserRootManage'),
-            meta: { title: '角色管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '角色管理', keepAlive: false, permission: [ 'role' ] }
           },
           {
             path: '/settings/staffManage',
             name: 'staffManage',
             component: () => import('@/views/settings/staffManage'),
-            meta: { title: '用户管理', keepAlive: false, permission: [ 'table' ] }
+            meta: { title: '用户管理', keepAlive: false, permission: [ 'user' ] }
           },
         ]
       },
