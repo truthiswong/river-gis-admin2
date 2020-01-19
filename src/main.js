00a8fa51b66618d037dcf035b07a6a33955f007e
wong <1457368987@qq.com>
2020-01-19 17:58:01 +0800

    格式化日期

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/'
-import { VueAxios } from './utils/request'
+import {
+  VueAxios
+} from './utils/request'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import splitPane from 'vue-splitpane'
@@ -14,6 +16,18 @@ import 'vue-video-player/src/custom-theme.css'
 import 'video.js/dist/video-js.css'
 import Viewer from 'v-viewer' // 查看图片
 import 'viewerjs/dist/viewer.css'
+
+// import moment from 'moment' // 格式化日期
+// // 全局过滤器 时间戳 
+// Vue.filter('dateformat', function (dataStr, pattern = 'YYYY-MM-DD') {
+//   if (dataStr) {
+//     return moment(dataStr).format(pattern)
+//   } else {
+//     return dataStr
+//   }
+// })
+// eg:
+// <span>发表时间：{{ item.date | dateformat('YYYY-MM-DD HH:mm:ss')}}</span>
 
 import bootstrap from './core/bootstrap'
 import './core/use'
@@ -33,8 +47,8 @@ Vue.use(Viewer, {
   }
 })
 Vue.use(ElementUI);
-Vue.component('split-pane',splitPane);
-Vue.component('draggable',draggable);
+Vue.component('split-pane', splitPane);
+Vue.component('draggable', draggable);
 
 new Vue({
   router,
