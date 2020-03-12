9fc835617a53e9eb61bc5dcba4fc66d0e8464c4a
truthiswong <1457368987@qq.com>
2020-03-12 09:34:56 +0800

    正射传参带项目id

--- src/views/supervise/Supervise.vue
+++ src/views/supervise/Supervise.vue
@@ -1341,7 +1341,7 @@ export default {
     let wordLabel = `http://t0.tianditu.com/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=a659a60049b130a5d1fececfd5a6b822`
     this.mapLayerWord = new T.TileLayer(wordLabel, { minZoom: 4, maxZoom: 18, zIndex: 15 })
     // 正射影像
-    this.mapImage = `${this.$store.state.serverUrl}/server/data/admin/regulator/uav/data/mbtiles?year=${this.mapYear}&month=${this.mapMonth}&day=${this.mapDay}&x={x}&y={y}&z={z}&X-TENANT-ID=${this.$store.state.tenantId}&Authorization=${token}`
+    this.mapImage = `${this.$store.state.serverUrl}/server/data/admin/regulator/uav/data/mbtiles?year=${this.mapYear}&month=${this.mapMonth}&day=${this.mapDay}&x={x}&y={y}&z={z}&X-TENANT-ID=${this.$store.state.tenantId}&projectId=${this.$store.state.id}&Authorization=${token}`
     this.mapLayerImage = new T.TileLayer(this.mapImage, { minZoom: 4, maxZoom: 23, zIndex: 12 })
     this.map = new T.Map('map', {
       minZoom: 4,
@@ -2609,7 +2609,7 @@ export default {
           picker[0]
         }&month=${picker[1]}&day=${picker[2]}&x={x}&y={y}&z={z}&X-TENANT-ID=${
           this.$store.state.tenantId
-        }&Authorization=${Vue.ls.get(ACCESS_TOKEN)}`
+        }&projectId=${this.$store.state.id}&Authorization=${Vue.ls.get(ACCESS_TOKEN)}`
         this.mapLayerImage = new T.TileLayer(mapImage, { minZoom: 4, maxZoom: 23, zIndex: 12 })
         this.map.addLayer(this.mapLayerImage)
       })
@@ -2738,7 +2738,7 @@ export default {
               this.mapYear
             }&month=${this.mapMonth}&day=${this.mapDay}&x={x}&y={y}&z={z}&X-TENANT-ID=${
               this.$store.state.tenantId
-            }&Authorization=${Vue.ls.get(ACCESS_TOKEN)}`
+            }&projectId=${this.$store.state.id}&Authorization=${Vue.ls.get(ACCESS_TOKEN)}`
             this.mapLayerImage = new T.TileLayer(mapImage, { minZoom: 4, maxZoom: 23, zIndex: 12 })
             this.map.addLayer(this.mapLayerImage)
             item.clicked = true
