752373769eaf65b99a5928a5f240f6e92f2d963f
wong <1457368987@qq.com>
2020-04-07 10:35:21 +0800

    风险源面积 做成可修改的

--- src/views/supervise/modules/AddRiskSource.vue
+++ src/views/supervise/modules/AddRiskSource.vue
@@ -156,7 +156,7 @@
           </a-col>
           <a-col :span="12">
             <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="面积(m²)">
-              <a-input placeholder v-model="list.currentArea" disabled />
+              <a-input placeholder v-model="list.currentArea" />
             </a-form-item>
           </a-col>
         </a-row>
